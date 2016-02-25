c1302dff5910c86cb7ba717b4c8ab0b72950ee1c
Student <student@mkelly2.local>
2016-02-25 10:52:31 -0600

    set up segue

---
+++ Breakout/GameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewController.swift
+//  Breakout
+//
+//  Created by mkelly2 on 2/25/16.
+//  Copyright © 2016 mkelly2. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- Breakout/ViewController.swift
+++ Breakout/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var breakOutLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let dvc = segue.destinationViewController as! GameViewController
+        dvc.title = breakOutLabel.text
     }
-
 
 }
 
