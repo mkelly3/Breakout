0c049c7942341690f20fff7988be52e07b87661c
Student <student@mkelly2.local>
2016-02-25 10:42:52 -0600

    Initial Commit

---
+++ Breakout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Breakout
+//
+//  Created by mkelly2 on 2/25/16.
+//  Copyright © 2016 mkelly2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
